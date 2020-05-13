da3587884efee7733528281b57d579e5c8ae3c31
Cédric Belin <cedric@belin.io>
2020-05-13 19:09:58 +0200

    Added the storage concrete classes

---
+++ src/webstorage/LocalStorage.hx
@@ -0,0 +1,12 @@
+package webstorage;
+
+import js.Browser;
+
+/** Provides access to the local storage. **/
+@:expose class LocalStorage extends WebStorage {
+  
+  /** Creates a new local storage service. **/
+  public function new(?options: WebStorage.WebStorageOptions) {
+    super(Browser.window.localStorage, options);
+  }
+}
---
+++ src/webstorage/SessionStorage.hx
@@ -0,0 +1,12 @@
+package webstorage;
+
+import js.Browser;
+
+/** Provides access to the session storage. **/
+@:expose class SessionStorage extends WebStorage {
+  
+  /** Creates a new session storage service. **/
+  public function new(?options: WebStorage.WebStorageOptions) {
+    super(Browser.window.sessionStorage, options);
+  }
+}
