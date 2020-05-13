00d7107e121119c28d59bdd2a5142d364e555fd1
Cédric Belin <cedric@belin.io>
2020-05-13 23:49:22 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -5,3 +5,7 @@ Set-Location (Split-Path $PSScriptRoot)
 tool/clean.ps1
 tool/version.ps1
 haxe build.hxml
+
+if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
+Copy-Item lib/webstorage.cjs build/webstorage.js
+node_modules/.bin/terser.ps1 --config-file=etc/terser.json --output=build/webstorage.min.js build/webstorage.js
