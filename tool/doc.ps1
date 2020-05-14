bc8ebbea80b8a13f7989a09d1f406cb1ac8ce107
Cédric Belin <cedric@belin.io>
2020-05-14 11:57:53 +0200

    Updated the "doc" task

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -3,8 +3,9 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
+haxe --define doc-gen --xml var/api.xml build.hxml
 haxelib run dox `
-  --define description 'Parse and format to LCOV your code coverage reports, in Haxe, JavaScript and PHP. The best way to share your code coverage stats.' `
+  --define description 'Services for interacting with the Web Storage, in Haxe and JavaScript. An event-based API to manage storage changes.' `
   --define logo 'https://api.belin.io/webstorage.hx/favicon.ico' `
   --define source-path 'https://git.belin.io/cedx/webstorage.hx/src/branch/master/src' `
   --define themeColor 0xffc105 `
