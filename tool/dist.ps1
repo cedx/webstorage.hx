ebb25f43618dfd75ddb6892c1150bf8e46b960f1
Cédric Belin <cedric@belin.io>
2020-05-24 01:26:17 +0200

    Fixed the "dist" task on Linux

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -8,4 +8,4 @@ haxe --no-traces build.hxml
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/webstorage.js build/webstorage.js
-node_modules/.bin/terser.ps1 --config-file=etc/terser.json --output=build/webstorage.min.js build/webstorage.js
+node_modules/.bin/terser --config-file=etc/terser.json --output=build/webstorage.min.js build/webstorage.js
