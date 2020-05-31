5d45a7dc5fd609bf7de5c7ce6d6a17c03da598ad
Cédric Belin <cedric@belin.io>
2020-06-01 01:03:14 +0200

    Code factorization

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -3,12 +3,17 @@ import haxe.ds.List;
 import utest.Assert;
 import webstorage.*;
 
-/** Runs the test suites. **/
+/** Runs the test suite. **/
 class TestAll {
+
+	/** The test cases. **/
+	static final tests = [
+		"WebStorage" => new WebStorageTest()
+	];
 
 	/** Application entry point. **/
 	static function main(): Void {
 		Assert.results = new List();
-		describe("WebStorage", new WebStorageTest().run);
+		for (description => test in tests) describe(description, test.run);
 	}
 }
