8471d55a03958af4933a33232df8b2f15d832945
Cédric Belin <cedric@belin.io>
2020-06-06 01:51:49 +0200

    Updated the "clean" task

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -6,6 +6,4 @@ foreach ($item in ".temp", "build", "doc/api", "lib/webstorage.*", "www") {
 	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
-foreach ($item in Get-ChildItem var -Exclude .gitkeep) {
-	Remove-Item $item -Recurse
-}
+Get-ChildItem var -Exclude .gitkeep | Remove-Item -Recurse
