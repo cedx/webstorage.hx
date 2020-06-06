aa7f27de80c4938fdf7538876a72adaee6069f79
Cédric Belin <cedric@belin.io>
2020-06-06 19:54:39 +0200

    Update the "dist" task

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -4,7 +4,7 @@ Set-Location (Split-Path $PSScriptRoot)
 
 tool/clean.ps1
 tool/version.ps1
-haxe --no-traces build.hxml
+haxe build.hxml
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/webstorage.js build/webstorage.js
