8bdbadde90552aa349840556ad42f681529e8237
Cédric Belin <cedric@belin.io>
2020-06-08 20:40:07 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -8,4 +8,4 @@ haxe build.hxml
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/webstorage.js build/webstorage.js
-node_modules/.bin/terser --config-file=etc/terser.json --output=build/webstorage.min.js build/webstorage.js
+npm run dist
