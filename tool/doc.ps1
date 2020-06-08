be9c2a4409435cc44154bacca2f752fb15b71b5f
Cédric Belin <cedric@belin.io>
2020-06-09 00:24:34 +0200

    Updated the documentation

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -7,7 +7,7 @@ haxe --define doc-gen --xml var/api.xml build.hxml
 haxelib run dox `
 	--define description "Services for interacting with the Web Storage, in Haxe and JavaScript. An event-based API to manage storage changes." `
 	--define logo "https://api.belin.io/webstorage.hx/favicon.ico" `
-	--define source-path "https://git.belin.io/cedx/webstorage.hx/src/branch/master/src" `
+	--define source-path "https://git.belin.io/cedx/webstorage.hx/src/branch/main/src" `
 	--define themeColor 0xffc105 `
 	--define version $version `
 	--define website "https://belin.io" `
