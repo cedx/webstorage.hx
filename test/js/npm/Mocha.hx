b599ee9f9ad2cdeb0a9c3d324e5a5c05cb1c94c9
Cédric Belin <cedric@belin.io>
2020-07-22 22:44:08 +0200

    Code formatting

--- test/js/npm/Mocha.hx
+++ test/js/npm/Mocha.hx
@@ -3,6 +3,15 @@ package js.npm;
 import haxe.extern.EitherType;
 import js.lib.Error;
 
+/** Callback function used for tests and hooks. **/
+typedef Callback = EitherType<DoneCallback, VoidCallback>;
+
+/** An error-first callback function. **/
+typedef DoneCallback = EitherType<() -> Void, Error -> Void> -> Void;
+
+/** A callback function. **/
+typedef VoidCallback = () -> Void;
+
 /** The Mocha test runner. **/
 @:native("window")
 extern class Mocha {
@@ -45,12 +54,3 @@ extern class Mocha {
 	@:native("it.skip")
 	static function itSkip(specification: String, callback: Callback): Void;
 }
-
-/** Callback function used for tests and hooks. **/
-typedef Callback = EitherType<DoneCallback, VoidCallback>;
-
-/** An error-first callback function. **/
-typedef DoneCallback = EitherType<() -> Void, Error -> Void> -> Void;
-
-/** A callback function. **/
-typedef VoidCallback = () -> Void;
