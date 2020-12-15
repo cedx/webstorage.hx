e606eb6104b2e96e796205f9f297ad04e02f6cf0
Cédric Belin <cedric@belin.io>
2020-12-15 19:10:56 +0100

    Using MS Edge as test browser

--- etc/karma.js
+++ etc/karma.js
@@ -1,6 +1,7 @@
+process.env.CHROME_BIN = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
 module.exports = config => config.set({
 	basePath: "..",
-	browsers: ["FirefoxHeadless"],
+	browsers: ["ChromeHeadless"],
 	files: ["var/tests.js"],
 	frameworks: ["mocha"],
 	reporters: ["progress"],
