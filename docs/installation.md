8f8356e032ce4afc5ce34f88de43a7e566fee123
Cédric Belin <cedric@belin.io>
2021-07-12 19:28:53 +0200

    Updated the documentation

--- docs/installation.md
+++ docs/installation.md
@@ -11,7 +11,7 @@ You can verify if you're already good to go with the following commands:
 #### **Haxe**
 ```shell
 haxe --version
-# 4.2.1
+# 4.2.3
 
 haxelib version
 # 4.0.2
--- example/main.hx
+++ example/main.hx
@@ -2,53 +2,49 @@ import js.Browser.console;
 import js.lib.Object;
 import webstorage.LocalStorage;
 
-/** A sample class. **/
-class Main {
+/** Application entry point. **/
+function main() {
+	final service = new LocalStorage();
 
-	/** Application entry point. **/
-	static function main() {
-		final service = new LocalStorage();
+	// Query the storage.
+	console.log(service.exists("foo")); // false
+	console.log(service.exists("baz")); // false
+	console.log(service.length); // 0
+	console.log(service.keys); // []
 
-		// Query the storage.
-		console.log(service.exists("foo")); // false
-		console.log(service.exists("baz")); // false
-		console.log(service.length); // 0
-		console.log(service.keys); // []
+	// Write to the storage.
+	service.set("foo", "bar");
+	console.log(service.exists("foo")); // true
+	console.log(service.length); // 1
+	console.log(service.keys); // ["foo"]
 
-		// Write to the storage.
-		service.set("foo", "bar");
-		console.log(service.exists("foo")); // true
-		console.log(service.length); // 1
-		console.log(service.keys); // ["foo"]
+	service.setObject("baz", {qux: 123});
+	console.log(service.exists("baz")); // true
+	console.log(service.length); // 2
+	console.log(service.keys); // ["foo", "baz"]
 
-		service.setObject("baz", {qux: 123});
-		console.log(service.exists("baz")); // true
-		console.log(service.length); // 2
-		console.log(service.keys); // ["foo", "baz"]
+	// Read the storage.
+	console.log(Std.isOfType(service.get("foo"), String)); // true
+	console.log(service.get("foo")); // "bar"
 
-		// Read the storage.
-		console.log(Std.isOfType(service.get("foo"), String)); // true
-		console.log(service.get("foo")); // "bar"
+	console.log(Std.isOfType(service.getObject("baz"), Object)); // true
+	console.log(service.getObject("baz")); // {qux: 123}
+	console.log(service.getObject("baz").qux); // 123
 
-		console.log(Std.isOfType(service.getObject("baz"), Object)); // true
-		console.log(service.getObject("baz")); // {qux: 123}
-		console.log(service.getObject("baz").qux); // 123
+	// Iterate the storage.
+	for (key => value in service) console.log('$key => $value');
 
-		// Iterate the storage.
-		for (key => value in service) console.log('$key => $value');
+	// Delete from the storage.
+	service.remove("foo");
+	console.log(service.exists("foo")); // false
+	console.log(service.length); // 1
+	console.log(service.keys); // ["baz"]
 
-		// Delete from the storage.
-		service.remove("foo");
-		console.log(service.exists("foo")); // false
-		console.log(service.length); // 1
-		console.log(service.keys); // ["baz"]
+	service.clear();
+	console.log(service.exists("baz")); // false
+	console.log(service.length); // 0
+	console.log(service.keys); // []
 
-		service.clear();
-		console.log(service.exists("baz")); // false
-		console.log(service.length); // 0
-		console.log(service.keys); // []
-
-		// Release the event listeners.
-		service.destroy();
-	}
+	// Release the event listeners.
+	service.destroy();
 }
