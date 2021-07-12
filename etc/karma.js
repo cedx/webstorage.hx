150a0c01829c6abb69a83db8086e06db8dedee9a
Cédric Belin <cedric@belin.io>
2021-07-12 19:27:44 +0200

    Updated the dependencies

--- etc/karma.js
+++
@@ -1,9 +0,0 @@
-process.env.CHROME_BIN = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
-module.exports = config => config.set({
-	basePath: "..",
-	browsers: ["ChromeHeadless"],
-	files: ["var/tests.js"],
-	frameworks: ["mocha"],
-	reporters: ["progress"],
-	singleRun: true
-});
