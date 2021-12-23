b2bd6dd49d138bcef3c0ee5c753439aa2a267063
Cédric Belin <cedric@belin.io>
2021-12-23 15:27:38 +0100

    Enabled the test colorization

--- test/RunTests.hx
+++ test/RunTests.hx
@@ -1,9 +1,17 @@
 import js.Syntax;
+import tink.testrunner.Reporter.AnsiFormatter;
+import tink.testrunner.Reporter.BasicReporter;
 import tink.testrunner.Runner;
 import tink.unit.TestBatch;
 
 /** Runs the test suite. **/
 function main() {
-	final tests = TestBatch.make([new webstorage.StorageTest()]);
-	Runner.run(tests).handle(result -> Syntax.code("exit({0})", result.summary().failures.length));
+	final tests = TestBatch.make([
+		new webstorage.StorageTest()
+	]);
+
+	ANSI.stripIfUnavailable = false;
+	Runner
+		.run(tests, new BasicReporter(new AnsiFormatter()))
+		.handle(outcome -> Syntax.code("exit({0})", outcome.summary().failures.length));
 }
