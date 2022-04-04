2dea7834650bee781097f7ff7461ba96e9865f85
Cédric Belin <cedric@belin.io>
2022-04-05 01:53:29 +0200

    Code optimization [skip ci]

--- src/webstorage/SessionStorage.hx
+++ src/webstorage/SessionStorage.hx
@@ -3,9 +3,7 @@ package webstorage;
 import js.Browser.window;
 
 /** Provides access to the session storage. **/
-#if tink_json
 @:jsonStringify(storage -> [for (key => value in storage) key => value])
-#end
 final class SessionStorage extends Storage {
 
 	/** Creates a new session storage service. **/
