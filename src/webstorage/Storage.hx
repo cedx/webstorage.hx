6949cc92e1daf02b95e3254ab88109e4d34b20f4
Cédric Belin <cedric@belin.io>
2023-02-20 11:55:07 +0100

    Fixed the typings

--- src/webstorage/Storage.hx
+++ src/webstorage/Storage.hx
@@ -83,7 +83,7 @@ class Storage {
 		Gets the deserialized value associated with the specified `key`.
 		Returns `None` if the `key` does not exist or its value cannot be deserialized.
 	**/
-	public function getObject<T>(key: String): Option<T> {
+	public function getObject(key: String): Option<Dynamic> {
 		final value = backend.getItem(buildKey(key));
 		return value == null ? None : switch Error.catchExceptions(() -> Json.parse(value)) {
 			case Failure(_): None;
@@ -110,10 +110,11 @@ class Storage {
 		Returns the deserialized value associated with `key`, if there is one.
 		Otherwise calls `ifAbsent` to get a new value, serializes it and associates `key` with that value, and then returns the new value.
 	**/
-	public function putObjectIfAbsent<T>(key: String, ifAbsent: () -> T) return switch getObject(key) {
-		case Some(value): Success(value);
-		case None: final value = ifAbsent(); setObject(key, value).map(_ -> value);
-	}
+	public function putObjectIfAbsent(key: String, ifAbsent: () -> Any): Outcome<Dynamic, Error>
+		return switch getObject(key) {
+			case Some(value): Success(value);
+			case None: final value = ifAbsent(); setObject(key, value).map(_ -> value);
+		}
 
 	/**
 		Removes the value associated with the specified `key`.
@@ -136,7 +137,7 @@ class Storage {
 		}, exception -> Error.withData(InsufficientStorage, "The storage is full.", exception));
 
 	/** Serializes and associates a given `value` with the specified `key`. **/
-	public function setObject<T>(key: String, value: T): Outcome<Noise, Error>
+	public function setObject(key: String, value: Any): Outcome<Noise, Error>
 		return switch Error.catchExceptions(() -> Json.stringify(value)) {
 			case Failure(_): Failure(new Error(UnprocessableEntity, "Unable to encode the specified value in JSON."));
 			case Success(json): set(key, json);
