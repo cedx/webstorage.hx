badbf84b3407b26c65617ca6f14239d04ba5f403
Cédric Belin <cedric@belin.io>
2023-04-14 12:18:42 +0200

    Code optimization

--- test/webstorage/StorageTest.hx
+++ test/webstorage/StorageTest.hx
@@ -32,7 +32,7 @@ using StringTools;
 
 		// It should handle the key prefix.
 		keys = Storage.session({keyPrefix: "prefix:"}).keys;
-		asserts.assert(keys.length == 1 && keys[0] == "baz");
+		asserts.assert(keys.length == 1 && keys.pop() == "baz");
 
 		return asserts.done();
 	}
